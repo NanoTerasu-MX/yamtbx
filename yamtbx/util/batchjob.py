e97c7d40babefd8c0f29f2a3d7789b3cf62853ed
Fukuda-akiya <140470087+Fukuda-akiya@users.noreply.github.com>
2025-08-21 10:57:21 +0900

    Add files via upload

--- yamtbx/util/batchjob.py
+++ yamtbx/util/batchjob.py
@@ -480,8 +480,7 @@ class AOBA(JobManager):
         script_name =j.script_name
         wdir = j.wdir
 
-        cmd = f'ssh sfront 
-                "cd {wdir} && /opt/nec/nqsv/bin/qsub {script_name}"'
+        cmd = f'ssh sfront "cd {wdir} && /opt/nec/nqsv/bin/qsub {script_name}"'
         print(f"[DEBUG] CMD: {cmd}")
 
         p = subprocess.Popen(cmd, shell=True, cwd=wdir,
